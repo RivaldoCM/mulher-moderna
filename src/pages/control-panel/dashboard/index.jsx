e27b53b61956e90bdba304dd1aa5291fe16d9951
RivaldoCM <rivaldocardoso2010@hotmail.comM>
2022-08-02 19:48:14 -0300

    colocando em pratica funcionalidade dinamica do Card

--- src/pages/control-panel/dashboard/index.jsx
+++ src/pages/control-panel/dashboard/index.jsx
@@ -3,14 +3,51 @@ import { Card } from "../../../components/card";
 
 
 import { Container, Main } from "../styles";
-import { CardTitle, Stats, Title } from "./styles";
-import { CardContainer } from "../../../components/card/styles";
+import { Stats, Title } from "./styles";
+import { CardContainer, CardTitle } from "../../../components/card/styles";
 
 export function Dashboard(){
     return(
         <Container>
             <Menu />
             <Main>
+                <Title>
+                    <h2>Bem vindo(a), seila quem!</h2>
+                    <p>Veja aqui suas estátisticas</p>
+                </Title>
+                <Stats>
+
+                </Stats>
+                <section className="section__item">
+                    <CardTitle>
+                        <h3>O que os clientes mais procuram</h3>
+                    </CardTitle>
+                    <CardContainer justify="start" padding="2rem 0">
+                        <Card />
+                        <Card />
+                        <Card />
+                    </CardContainer>
+                </section>
+                <section className="section__item">
+                    <CardTitle>
+                        <h3>O que os clientes odeiam nessa porra</h3>
+                    </CardTitle>
+                    <CardContainer justify="start" padding="2rem 0">
+                        <Card />
+                        <Card />
+                        <Card />
+                    </CardContainer>
+                </section>
+                <section className="section__item">
+                    <CardTitle>
+                        <h3>Melhores produtos da loja</h3>
+                    </CardTitle>
+                    <CardContainer justify="start" padding="2rem 0">
+                        <Card />
+                        <Card />
+                        <Card />
+                    </CardContainer>
+                </section>
             </Main>
         </Container>
     )
